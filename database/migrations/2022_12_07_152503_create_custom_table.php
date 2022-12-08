c386b44e73b558f74ab1a0a284b9084b696be582
johnny Huang <johnny8947276@gmail.com>
2022-12-08 16:33:29 +0800

    ##

--- database/migrations/2022_12_07_152503_create_custom_table.php
+++ database/migrations/2022_12_07_152503_create_custom_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('custom', function (Blueprint $table) {
             $table->id();
-            $table->string('email')->unique();
+            $table->string('email',20)->unique();
             $table->string('password', 20);
             $table->string('name', 50);
             $table->string('nickname', 20)->nullable();
